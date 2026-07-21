6ccff243d4b58a4ff97d6cf7fe812b039a6dae3a
anastasia-pspc <anastasia.sadovskyy@tpsgc-pwgsc.gc.ca>
2026-07-21 19:29:25 +0000

    Edited my title

--- client/pages/Anastasia.tsx
+++ client/pages/Anastasia.tsx
@@ -9,7 +9,7 @@ export default function Anastasia() {
       <main className="px-4 py-6">
         <div className="max-w-4xl mx-auto text-left">
           <h1 className="text-ado-text font-inter text-4xl font-bold mb-12 text-center">
-            🎀 Anastasia's Project
+            🎀 Anastasia's Project 🎀
           </h1>
         </div>
       </main>
