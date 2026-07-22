cb9d0d72087c08a67515569631a54b38c5c87fab
anastasia-pspc <anastasia.sadovskyy@tpsgc-pwgsc.gc.ca>
2026-07-22 12:35:19 +0000

    Anastasia - fixed a bug

--- client/pages/Anastasia.tsx
+++ client/pages/Anastasia.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import e from 'express';
 
