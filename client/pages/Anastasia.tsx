b75f7be89d338d3302066e605c0305c39005906a
anastasia-pspc <anastasia.sadovskyy@tpsgc-pwgsc.gc.ca>
2026-07-22 12:38:16 +0000

    Again

--- client/pages/Anastasia.tsx
+++ client/pages/Anastasia.tsx
@@ -1,8 +1,8 @@
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import e from 'express';
+import { useState } from 'react';
+//import { Link } from 'react-router-dom';
+//import e from 'express';
 
 export default function Anastasia() {
 
