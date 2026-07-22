2dbad6ad2a53109969cf2fa6c8b4f19ce5568b78
anastasia-pspc <anastasia.sadovskyy@tpsgc-pwgsc.gc.ca>
2026-07-22 12:32:17 +0000

    Anastasia - playing around with my UI page

--- client/pages/Anastasia.tsx
+++ client/pages/Anastasia.tsx
@@ -1,8 +1,12 @@
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import { Link } from 'react-router-dom';
+import e from 'express';
 
 export default function Anastasia() {
+
+  const [name, setName] = useState('');
+
   return (
     <div className="min-h-screen bg-ado-bg">
       <Header />
@@ -11,6 +15,21 @@ export default function Anastasia() {
           <h1 className="text-ado-text font-inter text-4xl font-bold mb-12 text-center">
             🎀 Anastasia's Project 🎀
           </h1>
+
+          <div className="max-w-md mx-auto>">
+            <label className="block mb-2 font-semibold">
+              Enter your name:
+            </label>
+            <input type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Type here..."
+            className="w-full p-2 border rounded"
+            />
+            <p className="mt-4">
+              You entered: {name}
+            </p>
+          </div>
         </div>
       </main>
       <Footer />
